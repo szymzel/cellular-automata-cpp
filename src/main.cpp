decd5a1b621ccdfb65749c4890d0d17ffb1d21f9
Szymon Zelik <szymonzelik@MacBook-Air-Szymon.local>
2026-09-13 14:48:08 +0200

    Now you can set cellular automaton and initial conditions through terminal

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,56 @@ const int cellSize = 6;
 
 bool IsPaused = 0;
 
-int main(){
+int main(int argc, char* argv[]){
+    std::cout << "available cellular automata: game_of_life, seeds, high_life\n";
+    std::cout << "available initial conditions: acorn, pulsar, default_scenario, glider_gun, random, all_colored_board\n";
+    std::cout << "use: " << argv[0] << " [automaton] [pattern]  (or without arguments - it will ask interactively)\n\n";
 
-    std::unique_ptr<cellular_automaton> automaton = std::make_unique<high_life>("High Life");
+    std::string automaton_name = "game_of_life";
+    std::string pattern_name = "random";
 
+    if (argc > 1){
+        automaton_name = argv[1];
+    } else {
+        std::cout << "pick automaton: ";
+        std::cin >> automaton_name;
+    }
 
+    if (argc > 2){
+        pattern_name = argv[2];
+    } else {
+        std::cout << "pick initial conditions: ";
+        std::cin >> pattern_name;
+    }
+
+    std::unique_ptr<cellular_automaton> automaton;
+    if (automaton_name == "game_of_life"){
+        automaton = std::make_unique<game_of_life>("Conway");
+    } else if (automaton_name == "seeds"){
+        automaton = std::make_unique<seeds>("Seeds");
+    } else if (automaton_name == "high_life"){
+        automaton = std::make_unique<high_life>("High Life");
+    }
 
     std::string name = automaton->getName();
     InitWindow(W*cellSize,H*cellSize, name.c_str());
     SetTargetFPS(60);
     board grid(W,H);
-    Random(grid);
+
+    if (pattern_name == "acorn"){
+        Acorn(grid);
+    } else if (pattern_name == "pulsar"){
+        Pulsar(grid);
+    } else if (pattern_name == "default_scenario"){
+        DefaultScenario(grid);
+    } else if (pattern_name == "glider_gun"){
+        GliderGun(grid);
+    } else if (pattern_name == "random"){
+        Random(grid);
+    } else if (pattern_name == "all_colored_board"){
+        AllColoredBoard(grid);
+    }
+    
     int GenerationNumber = 0;
     while (!WindowShouldClose()){
         handleInput(grid, cellSize);
